9df03054247e222673de42afbd5460e88d1e9026
hex <yiwen9999@live.cn>
2019-10-24 16:55:57 +0800

    mac 2019-10-23 14:20 框架变更 版本依赖调整 新增 配置通过git仓库统一配置中心获取后，添加自动刷新配置修改功能

--- server/src/main/java/com/hex/springcloudorder/client/ProductClient.java
+++ server/src/main/java/com/hex/springcloudorder/client/ProductClient.java
@@ -2,7 +2,7 @@ package com.hex.springcloudorder.client;
 
 import com.hex.springcloudorder.dataobject.ProductInfo;
 import com.hex.springcloudorder.dto.CartDTO;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
